1a9c900c9975de07162149d3bb044cab293a87c7
xiaowei <goodMorning_pro@atguigu.com>
2020-01-15 10:55:02 +0800

    测试一下Optional功能

--- Module01/src/com/atguigu/bean/Customer.java
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -1,8 +1,14 @@
 package com.atguigu.bean;
 
+import java.util.Optional;
+
 public class Customer {
     public static void main(String[] args){
 
-        System.out.println("helloworld!!");
+        String aaa="aaaaaa";
+     //   Optional.ofNullable(args);
+
+        System.out.println(Optional.ofNullable(aaa));
     }
+
 }
